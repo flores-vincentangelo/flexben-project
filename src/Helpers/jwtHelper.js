51482eebf5c49c5667902c217996b720618b0c1d
Vincent Flores <94419368+flores-vincentangelo@users.noreply.github.com>
2022-09-16 14:10:07 +0800

    updates

--- src/Helpers/jwtHelper.js
+++ src/Helpers/jwtHelper.js
@@ -1,4 +1,4 @@
-let DbUsers = require("../DataAccess/Database/DbAccounts");
+const DbEmployees = require("../DataAccess/Database/DbEmployees");
 let { JWT_OPTIONS } = require("../env/constants");
 const jwt = require("jsonwebtoken");
 const responses = require("../Helpers/responsesHelper");
@@ -22,16 +22,30 @@ function getAudienceFromToken(token) {
 
 async function generateToken(prevToken, userEmail) {
 	const email = userEmail || getEmployeeEmailFromToken(prevToken);
-	const account = await DbUsers.getAccountByEmployeeEmail(email);
+	const employee = await DbEmployees.getEmployeeDetailsByEmail(email);
+
+	let audience;
+	switch (employee.Role) {
+		case "employee":
+			audience = JWT_OPTIONS.EMPLOYEE_AUDIENCE;
+			break;
+		case "hr":
+			audience = JWT_OPTIONS.HR_AUDIENCE;
+			break;
+		case "payroll":
+			audience = JWT_OPTIONS.PAYROLL_AUDIENCE;
+			break;
+
+		default:
+			audience = [];
+			break;
+	}
 	const options = {
 		algorithm: process.env.ALGORITHM,
 		expiresIn: process.env.EXPIRY,
 		issuer: process.env.ISSUER,
-		subject: userEmail || account.Email,
-		audience: ["CHANGE ME LATER"],
-		// user[0].role === "staff"
-		// 	? JWT_OPTIONS.STAFF_AUDIENCE
-		// 	: JWT_OPTIONS.CUSTOMER_AUDIENCE,
+		subject: userEmail || employee.Email,
+		audience: audience,
 	};
 	return jwt.sign({}, process.env.SECRET, options);
 }
--- src/Routes/EmployeeRoutes.js
+++ src/Routes/EmployeeRoutes.js
@@ -1,19 +1,32 @@
 const responses = require("../Helpers/responsesHelper");
 const DbEmployees = require("../DataAccess/Database/DbEmployees");
 const jwtHelper = require("../Helpers/jwtHelper");
+const { AUDIENCE_OPTIONS } = require("../env/constants");
 
 let EmployeeRoutes = { getDetails };
 module.exports = EmployeeRoutes;
 
 async function getDetails(req, res, next) {
-	let email = jwtHelper.getEmployeeEmailFromToken(req.cookies.token);
-	let employee = await DbEmployees.getEmployeeDetailsByEmail(email);
-	res.status(200).json({
-		...responses.OkResponseBuilder("OK"),
-		data: {
-			firstName: employee.FirstName,
-			lastName: employee.LastName,
-			role: employee.Role,
-		},
-	});
+	try {
+		if (
+			jwtHelper
+				.getAudienceFromToken(req.cookies.token)
+				.includes(AUDIENCE_OPTIONS.EMPLOYEE_DETAILS)
+		) {
+			let email = jwtHelper.getEmployeeEmailFromToken(req.cookies.token);
+			let employee = await DbEmployees.getEmployeeDetailsByEmail(email);
+			res.status(200).json({
+				...responses.OkResponseBuilder("OK"),
+				data: {
+					firstName: employee.FirstName,
+					lastName: employee.LastName,
+					role: employee.Role,
+				},
+			});
+		} else {
+			res.status(403).json(responses.forbiddenResponse);
+		}
+	} catch (error) {
+		next(error);
+	}
 }
